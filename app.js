037291d7a7d2c05f3b51c9ecb532ef33898cba8c
Elisberto Junior <elisberto_jr@hotmail.com>
2022-10-21 22:24:03 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const clockContainer = document.querySelector('.clock-container')
+
+const formatTimeUnit = unit => String(unit).length === 1 ? `0${unit}` : unit
+
+const getClockHTML = (hours, minutes, seconds) => {
+  clockContainer.innerHTML = `
+    <span>${hours}</span> :
+    <span>${minutes}</span> :
+    <span>${seconds}</span>  
+  `
+}
+
+const updateClock = () => {
+  const present = new Date()
+  const hours = formatTimeUnit(present.getHours())
+  const minutes = formatTimeUnit(present.getMinutes())
+  const seconds = formatTimeUnit(present.getSeconds())
+
+  getClockHTML(hours, minutes, seconds)
+}
+
+setInterval(updateClock, 1000)
